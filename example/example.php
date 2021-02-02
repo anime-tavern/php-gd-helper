7d1fc3f0c7f92ef93b1ab3532558eaf22ac54758
nox7 <17110935+nox7@users.noreply.github.com>
2021-02-02 11:09:27 -0600

    Update example.php

--- example/example.php
+++ example/example.php
@@ -3,3 +3,6 @@
 
 	$imagePath = __DIR__ . "/example.png";
 	$gdHelper = GDHelper::fromFilePath($imagePath, IMAGETYPE_PNG);
+	$croppedGDHelper = $gdHelper->cropFromCenter(300,300);
+?>
+<img src="<?= $croppedGDHelper->toBase64DataString() ?>">
