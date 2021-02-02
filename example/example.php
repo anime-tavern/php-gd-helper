4be8ae5ebdbc922adff19166ac20ae0b3090cbda
nox7 <17110935+nox7@users.noreply.github.com>
2021-02-02 11:29:20 -0600

    Update example.php

--- example/example.php
+++ example/example.php
@@ -4,5 +4,16 @@
 	$imagePath = __DIR__ . "/example.png";
 	$gdHelper = GDHelper::fromFilePath($imagePath, IMAGETYPE_PNG);
 	$croppedGDHelper = $gdHelper->cropFromCenter(300,300);
+	$rotatedImage = $croppedGDHelper->rotate(90);
 ?>
+<strong>Original image</strong>
+<br>
+<img src="<?= $gdHelper->toBase64DataString() ?>">
+<br>
+<strong>Cropped image</strong>
+<br>
 <img src="<?= $croppedGDHelper->toBase64DataString() ?>">
+<br>
+<strong>Rotated image</strong>
+<br>
+<img src="<?= $rotatedImage->toBase64DataString() ?>">
