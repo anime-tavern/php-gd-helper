2f77f031a396735c905b805147e28cf6c5c24fc5
nox7 <17110935+nox7@users.noreply.github.com>
2021-02-03 11:21:46 -0600

    Update example to error with animated webp

--- example/example.php
+++ example/example.php
@@ -4,16 +4,20 @@
 	$pngImagePath = __DIR__ . "/example.png";
 	$gifImagePath = __DIR__ . "/example.gif";
 	$gif2ImagePath = __DIR__ . "/example-2.gif";
-	$gdHelper = GDHelper::fromFilePath($pngImagePath);
+	$webpImagePath = __DIR__ . "/example.webp";
+	// $gdHelper = GDHelper::fromFilePath($pngImagePath);
+	//
+	// $gdHelper_gif = GDHelper::fromFilePath($gifImagePath);
+	// $rotatedGif = $gdHelper_gif->rotate(45);
+	// $rotatedGifBase64 = $rotatedGif->toBase64DataString();
+	// $gdHelper_gif->clearResource();
+	// $rotatedGif->clearResource();
 
-	$gdHelper_gif = GDHelper::fromFilePath($gifImagePath);
-	$rotatedGif = $gdHelper_gif->rotate(45);
+	$gdHelper_webp = GDHelper::fromFilePath($webpImagePath);
+	//$roatedWebP = $gdHelper_webp->rotate(90);
 
-	$gdHelper_gif2 = GDHelper::fromFilePath($gif2ImagePath);
-	$rotatedGif2 = $gdHelper_gif2->rotate(-45);
-
-	$croppedGDHelper = $gdHelper->cropFromCenter(300,300);
-	$rotatedImage = $croppedGDHelper->rotate(90);
+	// $croppedGDHelper = $gdHelper->cropFromCenter(300,300);
+	// $rotatedImage = $croppedGDHelper->rotate(90);
 ?>
 <strong>Original image</strong>
 <br>
@@ -30,3 +34,5 @@
 <img src="<?= $rotatedGif->toBase64DataString() ?>">
 <br>
 <img src="<?= $rotatedGif2->toBase64DataString() ?>">
+<br>
+<img src="<?= $roatedWebP->toBase64DataString() ?>">
